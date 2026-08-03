691eac28183fc96cf70706d6764edc1a0057bae8
Danng <futuretech.coder4@gmail.com>
2026-08-02 20:25:29 -0500

    Update loading card

--- components/LoadingCard.tsx
+++ components/LoadingCard.tsx
@@ -6,11 +6,17 @@ import {useEffect, useState} from "react";
 
 const LOADING_MESSAGES = [
     "Heating the oven...",
-    "Pondering the life the universe and everything...",
+    "Pondering life the universe and everything...",
+    "Thinking...",
     "Going back to assigned task...",
     "Generating site...",
     "Almost there...",
     "Just a little longer...",
+    "Come on, you can wait another couple seconds...",
+    "Taking a while huh?",
+    "I wonder why it's taking this long...",
+    "Heating the oven...",
+    "HAH, gotcha, bet you thought it was going to restart!"
 ]
 
 export default function LoadingCard(){
@@ -19,14 +25,14 @@ export default function LoadingCard(){
     useEffect(() => {
         const interval = setInterval(() => {
             setMessageIndex((prev) => (prev + 1) % LOADING_MESSAGES.length);
-        }, 2000); // swap every 2s
+        }, 2500); // swap every 2s
 
         return () => clearInterval(interval);
     }, []);
 
     return (
         <div className="w-full min-h-screen flex flex-col items-center justify-center ">
-            <Card className="w-1/6 h-1/5">
+            <Card className="w-1/5 h-1/4">
                 <CardHeader>
                     <CardTitle>
                         <div className="flex flex-col justify-between">
